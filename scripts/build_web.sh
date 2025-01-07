a2904832007bda7e02d8d34a601cbc1d5dc08968
Anass Arakia <anass.arakia@tractive.com>
2025-01-07 12:36:39 +0100

    * Improved build web script * Updated readme

--- scripts/build_web.sh
+++ scripts/build_web.sh
@@ -1,8 +1,26 @@
-cd ../web
+#!/bin/bash
+
+# Navigate to the web directory
+cd ../web || exit
+
+# Install dependencies
 yarn install
+
+# Set environment variable (specific to macOS)
 export REACT_APP_API_BASE_URL="/dashboard"
+
+# Build the project
 yarn run build
+
+# Navigate back to the root directory
 cd ..
+
+# Remove the existing public directory
 rm -rf lib/public
+
+# Create the public directory
 mkdir -p lib/public
-cp -R ./web/build/ ./lib/public/
+
+# Copy the build output to the public directory
+cp -R ./web/build/* ./lib/public/
+
