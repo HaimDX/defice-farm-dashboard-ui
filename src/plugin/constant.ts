668800e0164dc32a88b5e0720d5cc647b289b653
Anass Arakia <anass.arakia@tractive.com>
2025-02-11 13:16:10 +0100

    * Fixed device farm apps paths

--- src/plugin/constant.ts
+++ src/plugin/constant.ts
@@ -1,2 +1,2 @@
-export const IOS_STAGE_APP_PATH = "/Users/ana/Downloads/gps.ipa";
-export const ANDROID_STAGE_APP_PATH = "/Users/ana/Downloads/gps.apk";
+export const IOS_STAGE_APP_PATH = "/Users/tractive/jenkins/apps/ios/stage/app.ipa";
+export const ANDROID_STAGE_APP_PATH = "/Users/tractive/jenkins/apps/android/stage/app.apk";
